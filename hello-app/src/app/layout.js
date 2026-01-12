1f818c63274311be975580dcc8fc6b51e617fb03
subugee <sasubramanian_md@hotmail.com>
2026-01-12 13:10:34 +0530

    core routing

---
+++ hello-app/src/app/layout.js
@@ -0,0 +1,17 @@
+
+// export default function RootLayout(props) {
+
+//   return <html lang="en">
+//     <body>
+//       {props.children}
+//     </body>
+//   </html>
+// }
+export default function RootLayout({children}) {
+
+  return <html lang="en">
+    <body>
+      {children}
+    </body>
+  </html>
+}
