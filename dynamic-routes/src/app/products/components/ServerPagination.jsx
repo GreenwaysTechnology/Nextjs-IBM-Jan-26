ca360889096fc94da6d7ace46caf0f3624ea0fa0
subugee <sasubramanian_md@hotmail.com>
2026-01-16 10:56:16 +0530

    dynmaic routes

--- dynamic-routes/src/app/products/components/ServerPagination.jsx
+++ dynamic-routes/src/app/products/components/ServerPagination.jsx
@@ -1,22 +1,24 @@
 import Link from 'next/link'
+import './styles.css'
 
 export default function ServerPagination({ currentPage, totalPages }) {
     console.log(currentPage)
     return (
         <nav>
-            <Link
-                href={`/products?page=${currentPage - 1}`}
-                aria-disabled={currentPage <= 1}
-            >
-                Previous ||
-            </Link>
-            <Link
-                href={`/products?page=${currentPage + 1}`}
-                aria-disabled={currentPage >= totalPages}
-                
-            >
-                Next
-            </Link>
+            {currentPage > 1 ? (
+                <Link href={`/products?page=${currentPage - 1}`}>
+                    Previous    |
+                </Link>
+            ) : (
+                <span className="disabled">Previous | </span>
+            )}
+            {currentPage < totalPages ? (
+                <Link href={`/products?page=${currentPage + 1}`}>
+                    Next
+                </Link>
+            ) : (
+                <span className="disabled">Next</span>
+            )}
             <div>
                 <span>Page {currentPage} of {totalPages}</span>
             </div>
