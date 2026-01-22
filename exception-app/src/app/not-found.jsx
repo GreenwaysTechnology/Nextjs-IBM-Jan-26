5610902eb0d03bd1f5cf1ad87e308d481ef34500
subugee <sasubramanian_md@hotmail.com>
2026-01-22 13:17:06 +0530

    exception app

---
+++ exception-app/src/app/not-found.jsx
@@ -0,0 +1,5 @@
+export default function NotFound(){
+    return <div>
+        <h1>The Requested Page not Available</h1>
+    </div>
+}
---
+++ exception-app/src/app/users/[id]/not-found.jsx
@@ -0,0 +1,5 @@
+export default function NotFound(){
+    return <div>
+        <h1>The Requested User is not Available</h1>
+    </div>
+}
