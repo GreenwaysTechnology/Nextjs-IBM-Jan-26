2d3969ce3221d18d24f559bf63d661f96cc4bdfb
subugee <sasubramanian_md@hotmail.com>
2026-01-23 12:00:48 +0530

    caching

---
+++ caching-isr/src/app/timecache/[city]/page.js
@@ -0,0 +1,20 @@
+
+//route segmentation options
+export const fetchCache = 'force-no-store'
+// export const fetchCache = 'force-cache'
+
+
+export async function getTime() {
+    //const url ='https://www.timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata'
+    const url = "http://localhost:8080/api/timezone/Asia/Kolkata"
+    //enable cache
+    const response = await fetch(url, { cache: 'force-cache' })
+    const data = await response.json()
+    return data
+}
+export default async function TimeCachePage() {
+    const data = await getTime()
+    return <div className="m-20">
+        <h1 className="font-bold">Time Now : {new Date(data.datetime).toLocaleTimeString()}</h1>
+    </div>
+}
