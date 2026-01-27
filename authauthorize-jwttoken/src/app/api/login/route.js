362e1412d557ae6b04b702c8925954d5736fb941
subugee <sasubramanian_md@hotmail.com>
2026-01-27 11:22:06 +0530

    authentication

---
+++ authauthorize-jwttoken/src/app/api/login/route.js
@@ -0,0 +1,24 @@
+import { cookies } from "next/headers";
+// import { signJWT } from "../../lib/jwt";
+import { signJWT } from '@/app/lib/jwt'
+
+export async function POST(req) {
+    const { username, password } = await req.json();
+    // ✅ Replace with real authentication
+    if (username === "admin" && password === "123") {
+        // Create session token (in real apps use JWT or DB session id)
+        const token = await signJWT({ username, role: "admin" });
+        const cookieStore = await cookies()
+        cookieStore.set("session", token, {
+            httpOnly: true,   // cannot be accessed by JS
+            secure: process.env.NODE_ENV === "production",    // only over HTTPS
+            sameSite: "strict",
+            path: "/",
+            maxAge: 60 * 60,  // 1 hour
+        });
+        return Response.json({ success: true });
+    }
+
+    return Response.json({ success: false, message: "Invalid credentials" }, { status: 401 });
+}
+
---
+++ authauthorize-jwttoken/src/app/login/page.js
@@ -0,0 +1,7 @@
+import Login from "@/app/login/components/Login";
+
+export default function LoginPage() {
+    return <div>
+        <Login />
+    </div>
+}
